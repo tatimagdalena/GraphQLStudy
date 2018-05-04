7999989a7e90d5c861d2d1f515d2d5e0c94c9c8d
Tatiana <tatimagdalena@gmail.com>
2018-05-04 09:16:58 -0300

    Using the query

---
+++ POCApolloGraphQL/GraphQLConsumer.swift
@@ -0,0 +1,25 @@
+//
+//  GraphQLConsumer.swift
+//  POCApolloGraphQL
+//
+//  Created by Tatiana Magdalena on 04/05/18.
+//  Copyright © 2018 Tatiana Magdalena. All rights reserved.
+//
+
+import Foundation
+import Apollo
+
+struct GraphQLConsumer {
+    
+    let apollo = ApolloClient(url: URL(string: "https://api.graphloc.com/graphql")!)
+    
+    func fetchLocation() {
+        apollo.fetch(query: LocationQueryQuery(ip: "189.59.228.170")) { (result, error) in
+            print(result?.data?.getLocation?.country?.geonameId ?? "")
+            print(result?.data?.getLocation?.country?.names?.en ?? "")
+            print(result?.data?.getLocation?.location?.latitude ?? "")
+            print(result?.data?.getLocation?.location?.longitude ?? "")
+        }
+    }
+    
+}
